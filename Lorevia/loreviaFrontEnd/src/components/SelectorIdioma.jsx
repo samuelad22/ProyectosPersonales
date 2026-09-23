e92017bf4506213adde511dd0ac90451d3404ee2
samuelad22 <samuelad22@educastur.es>
2026-09-23 21:36:07 +0200

    Primera version funcional - Testeado por usuario externo

--- Lorevia/loreviaFrontEnd/src/components/SelectorIdioma.jsx
+++ Lorevia/loreviaFrontEnd/src/components/SelectorIdioma.jsx
@@ -1,22 +1,30 @@
+import { useState } from 'react'
 import { useIdioma } from '../context/IdiomaContext'
 
 function SelectorIdioma() {
   const { idioma, cambiarIdioma, t } = useIdioma()
+  const [toogleAbierto, setToogleAbierto] = useState(false)
+
+  const cambiarEstadoToogle = () => {
+    setToogleAbierto((prev) => !prev)
+  }
 
   return (
-    <select
-      className="selector-idioma"
-      aria-label={t('idioma.etiqueta')}
-      value={idioma}
-      onChange={(evento) => cambiarIdioma(evento.target.value)}
-    >
-      <option value="es" lang="es">
-        {t('idioma.espanol')}
-      </option>
-      <option value="en" lang="en">
-        {t('idioma.ingles')}
-      </option>
-    </select>
+    <div className='contenedor-toogle-idioma'>
+      <button type="button" className={toogleAbierto == false ? '' : 'toogle'} onClick={cambiarEstadoToogle}> <img src= {idioma == "es" ? "https://flagcdn.com/es.svg" : "https://flagcdn.com/gb-eng.svg"} alt="Bandera de España" className="pais-bandera-foto" /> <span>{idioma == "es" ? "Español" : "English"}</span> </button>
+      <div className={toogleAbierto == false ? 'toogle' : ''}>
+        <div className="selector-idioma">
+          <div className="idioma" onClick={() => {cambiarIdioma('es'); setToogleAbierto(false)}}>
+            <img src="https://flagcdn.com/es.svg" alt="Bandera de España" className="pais-bandera-foto" />
+            <span>Español</span>
+          </div>
+          <div className="idioma" onClick={() => {cambiarIdioma('en'); setToogleAbierto(false)}}>
+            <img src="https://flagcdn.com/gb-eng.svg" alt="Bandera de Reino Unido" className="pais-bandera-foto" />
+            <span>English</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
